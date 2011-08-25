7c4088220fdf15a92e55cffc9adff8eb683e445a
BenObiWan <benobiwan@gmail.com>
2011-08-25 21:18:21 +0200

    Added game_launcher.

---
+++ src/game_launcher/src/game/launcher/Launcher.java
@@ -0,0 +1,234 @@
+package game.launcher;
+
+import game.core.ApplicationCore;
+import game.core.ui.CoreUI;
+import game.launcher.config.ILauncherConfiguration;
+import game.launcher.config.ILauncherConfigurationLoader;
+import game.launcher.config.LauncherPropertyConfigurationLoader;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
+
+import common.config.InvalidConfigurationException;
+
+/**
+ * Launcher class for the game application.
+ * 
+ * Loads the configuration, then, if it's specified on the configuration loads
+ * the GUI or act in server mode.
+ * 
+ * @author benobiwan
+ * 
+ */
+public final class Launcher
+{
+	/**
+	 * Default configuration directory if nothing is specified on the command
+	 * line.
+	 */
+	public static final String DEFAULT_CONFIGURATION_DIRECTORY = "/home/benobiwan/.benobiwan/";
+
+	/**
+	 * Option used to change the configuration directory.
+	 */
+	public static final String OPTION_CONF = "conf";
+
+	/**
+	 * Option used to display the help message.
+	 */
+	public static final String OPTION_HELP = "h";
+
+	/**
+	 * Option used to pass the application in server mode.
+	 */
+	public static final String OPTION_SERVER = "s";
+
+	/**
+	 * List of all the options that can be read on the command line. Not
+	 * including option directly passed to the JVM.
+	 */
+	private final Options _optionsList = new Options();
+
+	/**
+	 * Parser used to parse the options given on the command line.
+	 */
+	private final CommandLineParser _parser = new PosixParser();
+
+	/**
+	 * Help formatter used to display the help message.
+	 */
+	private final HelpFormatter _formatter = new HelpFormatter();
+
+	/**
+	 * Object describing which options have been read on the command line.
+	 */
+	private CommandLine _commandLine;
+
+	/**
+	 * Configuration directory that will be used.
+	 */
+	@SuppressWarnings("unused")
+	private String _strConfigurationPath;
+
+	/**
+	 * The {@link ILauncherConfiguration} of this program.
+	 */
+	private ILauncherConfiguration _launcherConf;
+
+	/**
+	 * Creates a new Launcher.
+	 */
+	public Launcher()
+	{
+		_optionsList.addOption(new Option(OPTION_CONF, true,
+				"Configuration directory."));
+		_optionsList
+				.addOption(new Option(OPTION_SERVER, false, "server mode."));
+	}
+
+	/**
+	 * Read the command line and load the configuration.
+	 * 
+	 * @param args
+	 *            the command line arguments.
+	 * @throws ParseException
+	 *             if an error in encountered during the parsing or if the help
+	 *             is asked for.
+	 * @throws InvalidConfigurationException
+	 */
+	public void load(final String[] args) throws ParseException,
+			InvalidConfigurationException
+	{
+		parse(args);
+		loadLoggingConfiguration();
+		loadConfiguration();
+	}
+
+	/**
+	 * Parses command line arguments.
+	 * 
+	 * @param args
+	 *            the command line arguments.
+	 * @throws ParseException
+	 *             if an error in encountered during the parsing or if the help
+	 *             is asked for.
+	 */
+	private void parse(final String[] args) throws ParseException
+	{
+		_commandLine = _parser.parse(_optionsList, args);
+		if (_commandLine.hasOption(OPTION_HELP))
+		{
+			throw new ParseException("Help message asked for.");
+		}
+		if (_commandLine.hasOption(OPTION_CONF))
+		{
+			_strConfigurationPath = _commandLine.getOptionValue(OPTION_CONF);
+		}
+		else
+		{
+			_strConfigurationPath = DEFAULT_CONFIGURATION_DIRECTORY;
+		}
+		if (_commandLine.hasOption(OPTION_SERVER))
+		{
+			System.getProperties().setProperty(
+					"conf.launcher.launcher.IsServer", "true");
+		}
+	}
+
+	/**
+	 * Load the logging configuration.
+	 */
+	private void loadLoggingConfiguration()
+	{
+		// PatternLayout layout = new PatternLayout(
+		// "%d [%t] %-5p (%F:%L) %c - %m%n");
+		// WriterAppender app = new WriterAppender(layout,
+		// m_logWindow.getOutputStream());
+		final ConsoleAppender app = new ConsoleAppender(new PatternLayout(
+				"%d{ISO8601} [%t] %-5p %C:%L %x - %m%n"));
+
+		BasicConfigurator.configure(app);
+	}
+
+	/**
+	 * Load the {@link ILauncherConfiguration} of this program.
+	 * 
+	 * @throws InvalidConfigurationException
+	 *             one of the configuration value is invalid.
+	 */
+	private void loadConfiguration() throws InvalidConfigurationException
+	{
+		final ILauncherConfigurationLoader _launcherConfLoader = new LauncherPropertyConfigurationLoader(
+				ManagementFactory.getPlatformMBeanServer());
+		_launcherConf = _launcherConfLoader.getLauncherConfiguration();
+	}
+
+	/**
+	 * Print the help of this application.
+	 */
+	public void printHelp()
+	{
+		_formatter.printHelp("game", _optionsList);
+	}
+
+	/**
+	 * Start the application.
+	 * 
+	 * @throws IOException
+	 *             an IO error occurred during the activation of the network
+	 *             server.
+	 */
+	public void start() throws IOException
+	{
+		final ApplicationCore appCore = new ApplicationCore();
+
+		if (_launcherConf.isDaemon())
+		{
+			appCore.activateNetworkServer();
+		}
+		else
+		{
+			CoreUI.setLookAndFeel();
+			final LauncherFrame frame = new LauncherFrame(_launcherConf,
+					appCore);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+		}
+	}
+
+	/**
+	 * @param args
+	 *            the arguments given to the program.
+	 * @throws IOException
+	 *             an IO error occurred during the activation of the network
+	 *             server.
+	 * @throws InvalidConfigurationException
+	 *             one of the configuration value is invalid.
+	 */
+	public static void main(final String[] args) throws IOException,
+			InvalidConfigurationException
+	{
+		final Launcher appMain = new Launcher();
+		try
+		{
+			appMain.load(args);
+			appMain.start();
+		}
+		catch (final ParseException e)
+		{
+			appMain.printHelp();
+		}
+	}
+}
---
+++ src/game_launcher/src/game/launcher/LauncherFrame.java
@@ -0,0 +1,149 @@
+package game.launcher;
+
+import game.core.ApplicationCore;
+import game.core.ui.CoreUI;
+import game.launcher.config.ILauncherConfiguration;
+
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Frame used to launch the application. Displays 3 buttons: local game, host
+ * game and join game. Pressing any of those buttons close the frame and create
+ * a new {@link CoreUI} frame showing internal frames appropriate to the pressed
+ * button.
+ * 
+ * @author benobiwan
+ * 
+ */
+public final class LauncherFrame extends JFrame implements ActionListener
+{
+	/**
+	 * Action command for the local game button.
+	 */
+	private static final String LOCAL_GAME_ACTION_COMMAND = "local_game";
+
+	/**
+	 * Action command for the host game button.
+	 */
+	private static final String HOST_GAME_ACTION_COMMAND = "host_game";
+
+	/**
+	 * Action command for the join game button.
+	 */
+	private static final String JOIN_GAME_ACTION_COMMAND = "join_game";
+
+	/**
+	 * Logger object.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(LauncherFrame.class);
+
+	/**
+	 * serialVersionUID for Serialization.
+	 */
+	private static final long serialVersionUID = 7850068285755022675L;
+
+	/**
+	 * The {@link ILauncherConfiguration} of this program.
+	 */
+	@SuppressWarnings("unused")
+	private final ILauncherConfiguration _launcherConf;
+
+	/**
+	 * The application core.
+	 */
+	private final ApplicationCore _appCore;
+
+	/**
+	 * The core UI.
+	 */
+	private final CoreUI _coreUI;
+
+	/**
+	 * Creates a new LauncherFrame.
+	 * 
+	 * @param launcherConf
+	 *            the {@link ILauncherConfiguration} of this program.
+	 * @param appCore
+	 *            the application core.
+	 * @throws HeadlessException
+	 *             Thrown when code that is dependent on a keyboard, display, or
+	 *             mouse is called in an environment that does not support a
+	 *             keyboard, display, or mouse.
+	 */
+	public LauncherFrame(final ILauncherConfiguration launcherConf,
+			final ApplicationCore appCore) throws HeadlessException
+	{
+		super("Launcher");
+		_launcherConf = launcherConf;
+		_appCore = appCore;
+		_coreUI = new CoreUI(_appCore);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		final JPanel contPane = new JPanel(new GridLayout(3, 1, 15, 15));
+		final JButton jbLocal = new JButton("Local Game");
+		jbLocal.setActionCommand(LOCAL_GAME_ACTION_COMMAND);
+		jbLocal.addActionListener(this);
+		contPane.add(jbLocal);
+		final JButton jbHost = new JButton("Host Game");
+		jbHost.setActionCommand(HOST_GAME_ACTION_COMMAND);
+		jbHost.addActionListener(this);
+		contPane.add(jbHost);
+		final JButton jbJoin = new JButton("Join Game");
+		jbJoin.setActionCommand(JOIN_GAME_ACTION_COMMAND);
+		jbJoin.addActionListener(this);
+		contPane.add(jbJoin);
+		final Border bor = new CompoundBorder(new EmptyBorder(15, 25, 15, 25),
+				new CompoundBorder(new BevelBorder(BevelBorder.RAISED),
+						new EmptyBorder(15, 25, 15, 25)));
+		contPane.setBorder(bor);
+
+		setContentPane(contPane);
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent ae)
+	{
+		_coreUI.setVisible(true);
+		setVisible(false);
+		if (LOCAL_GAME_ACTION_COMMAND.equals(ae.getActionCommand()))
+		{
+			_coreUI.showCreateGameWindow();
+		}
+		else if (HOST_GAME_ACTION_COMMAND.equals(ae.getActionCommand()))
+		{
+			try
+			{
+				_coreUI.showCreateGameWindow();
+				_appCore.activateNetworkServer();
+			}
+			catch (final IOException ex)
+			{
+				LOGGER.error(ex);
+			}
+		}
+		else if (JOIN_GAME_ACTION_COMMAND.equals(ae.getActionCommand()))
+		{
+			_coreUI.showServerListWindow();
+		}
+		else
+		{
+			LOGGER.error("Action command " + ae.getActionCommand()
+					+ " unknown.");
+		}
+	}
+}
