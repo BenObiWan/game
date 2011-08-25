d32c979d43740270930526d52dfd507abf4155b9
BenObiWan <benobiwan@gmail.com>
2011-08-25 21:21:56 +0200

    Added game_core.

---
+++ src/game_core/src/game/core/ApplicationCore.java
@@ -0,0 +1,117 @@
+package game.core;
+
+import game.communication.IGameListDescription;
+import game.gameclient.LocalGameClient;
+import game.gameserver.LocalGameServer;
+import game.network.NetworkMain;
+import game.network.config.INetworkConfigurationLoader;
+import game.network.config.NetworkXMLFileConfigurationLoader;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * The application core.
+ * 
+ * @author benobiwan
+ * 
+ */
+public final class ApplicationCore
+{
+
+	/**
+	 * Local game server.
+	 */
+	private final LocalGameServer _locGameServer;
+
+	/**
+	 * Local game client.
+	 */
+	private final LocalGameClient _locGameClient;
+
+	/**
+	 * Network main.
+	 */
+	private final NetworkMain _netMain;
+
+	/**
+	 * Creates a new ApplicationCore.
+	 */
+	public ApplicationCore()
+	{
+		final Set<IGameListDescription> gameDescription = new TreeSet<IGameListDescription>();
+
+		_locGameServer = new LocalGameServer(gameDescription);
+		// TODO read name in configuration file
+		_locGameClient = new LocalGameClient("loc");
+		_locGameServer.registerGameClient(_locGameClient);
+		_locGameClient.registerGameServer(_locGameServer);
+		final INetworkConfigurationLoader netConfLoader = new NetworkXMLFileConfigurationLoader(
+				ManagementFactory.getPlatformMBeanServer());
+		_netMain = new NetworkMain(_locGameClient, _locGameServer,
+				netConfLoader);
+	}
+
+	/**
+	 * Activate the network server.
+	 * 
+	 * @throws IOException
+	 *             if an error occurred during the activation of the server.
+	 */
+	public void activateNetworkServer() throws IOException
+	{
+		_netMain.activateServer();
+	}
+
+	/**
+	 * Deactivate the network server.
+	 */
+	public void deactivateNetworkServer()
+	{
+		_netMain.deactivateServer();
+	}
+
+	/**
+	 * Stop the application.
+	 */
+	public void stop()
+	{
+		if (_netMain.isServerActivated())
+		{
+			_netMain.deactivateServer();
+		}
+		// TODO stop the application.
+	}
+
+	/**
+	 * Get the network main.
+	 * 
+	 * @return the network main.
+	 */
+	public NetworkMain getNetworkMain()
+	{
+		return _netMain;
+	}
+
+	/**
+	 * Get the local game server.
+	 * 
+	 * @return the local game server.
+	 */
+	public LocalGameServer getLocalGameServer()
+	{
+		return _locGameServer;
+	}
+
+	/**
+	 * Get the local game client.
+	 * 
+	 * @return the local game client.
+	 */
+	public LocalGameClient getLocalGameClient()
+	{
+		return _locGameClient;
+	}
+}
